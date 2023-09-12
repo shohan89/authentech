42e03dac7c303ad594de26154f6cac42fb614c43
Shohan Sharoar <maffin.killer007@gmail.com>
2023-09-12 22:23:54 +0600

    Firebase initialize complete

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBshw01RBiYOZSSjQdAFOT69i9bdiZ6Lwg",
+  authDomain: "authentech-9d8c6.firebaseapp.com",
+  projectId: "authentech-9d8c6",
+  storageBucket: "authentech-9d8c6.appspot.com",
+  messagingSenderId: "735200365941",
+  appId: "1:735200365941:web:e2c1b771fbbfa3f5d7d73c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
